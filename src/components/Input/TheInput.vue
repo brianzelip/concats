8ee89c4cffcde0667e3d1fdcaca0865275da3a60
Brian Zelip <bzelip@gmail.com>
2018-12-10 09:45:43 -0500

    copy over components from concats-web, but tweaked a bit to fit in the conventions of this vue-cli 3 scaffolded project

---
+++ src/components/Input/TheInput.vue
@@ -0,0 +1,99 @@
+<template>
+  <section>
+    <label for="fileInput">Select csv file:</label>
+    <input
+      @change="ingestCsv"
+      accept=".csv"
+      id="fileInput"
+      name="fileInput"
+      type="file"
+    >
+    <div v-if="csvInputHeaders.length > 0">
+      <p>Select the headers to concat in order:</p>
+      <div
+        :key="index"
+        v-for="(header, index) in csvInputHeaders"
+      >
+        <input
+          :id="`headers-${index}`"
+          :value="header"
+          name="headers"
+          type="checkbox"
+          v-model="userSelectedHeaders"
+        >
+        <label :for="`headers-${index}`">{{ header }}</label>
+      </div>
+    </div>
+    <div v-if="userSelectedHeaders.length > 0">
+      <p>user selected headers are:</p>
+      <ol>
+        <li
+          :key="index"
+          v-for="(header, index) in userSelectedHeaders"
+        >{{ header }}</li>
+      </ol>
+    </div>
+    <button
+      :disabled="userSelectedHeaders.length < 1"
+      @click="setCsvOutput"
+    >Submit</button>
+    <pre>{{ csvOutput }}</pre>
+  </section>
+</template>
+
+<script>
+import CSV from "csvtojson";
+
+export default {
+  data() {
+    return {
+      csvInput: "",
+      csvInputHeaders: [],
+      userSelectedHeaders: [],
+      csvAsJson: [],
+      csvOutput: ""
+    };
+  },
+  methods: {
+    ingestCsv(e) {
+      const vm = this;
+      const file = e.target.files[0];
+      const reader = new FileReader();
+
+      reader.readAsText(file);
+      reader.onload = function(event) {
+        vm.csvInput = event.target.result;
+        CSV()
+          .fromString(vm.csvInput)
+          .on("header", header => {
+            vm.setCsvInputHeaders(header);
+          })
+          .then(json => {
+            vm.setCsvAsJson(json);
+          });
+      };
+    },
+    setCsvInputHeaders(data) {
+      this.csvInputHeaders = data;
+    },
+    setCsvAsJson(data) {
+      this.csvAsJson = data;
+    },
+    setCsvOutput() {
+      this.csvOutput = this.csvAsJson
+        .reduce((acc, obj) => {
+          const keys = this.userSelectedHeaders;
+          var concattedString = "";
+          keys.forEach((key, index) => {
+            index !== keys.length - 1
+              ? (concattedString = concattedString.concat(`${obj[key]} `))
+              : (concattedString = concattedString.concat(obj[key]));
+          });
+          acc.push(concattedString);
+          return acc;
+        }, [])
+        .join("\n");
+    }
+  }
+};
+</script>
---
+++ src/components/global/TheFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer>
+    by
+    <a href="http://zelip.me">bzelip</a> |
+    <a href="https://github.com/brianzelip/concats">app source</a>
+  </footer>
+</template>
