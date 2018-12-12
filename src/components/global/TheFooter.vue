c92c75746b8fc36a019f25f4fc6c8be74747e1b0
Brian Zelip <bzelip@gmail.com>
2018-12-12 04:17:41 -0500

    require fs via script tag in index.html??? That seems icky and weird, BUT THE VUE COMPONENT CAN READ FROM FS so it actually works?! via this stack overflow answer: https://stackoverflow.com/a/43239325/2145103

--- src/components/global/TheFooter.vue
+++ src/components/global/TheFooter.vue
@@ -3,5 +3,18 @@
     by
     <a href="http://zelip.me">bzelip</a> |
     <a href="https://github.com/brianzelip/concats">app source</a>
+    <div>
+      <p>{{ outputFile }}</p>
+    </div>
   </footer>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      outputFile: fs.constants
+    };
+  }
+};
+</script>
