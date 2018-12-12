88a166c9fa8b605d9a04ab39da884c3848921d77
Brian Zelip <bzelip@gmail.com>
2018-12-12 14:01:20 -0500

    render version number from package.json in footer

--- src/components/global/TheFooter.vue
+++ src/components/global/TheFooter.vue
@@ -3,7 +3,19 @@
     <div>
       <hr>by
       <a href="http://zelip.me">bzelip</a> |
-      <a href="https://github.com/brianzelip/concats">app source</a>
+      <a href="https://github.com/brianzelip/concats">app source</a> | v{{ version }}
     </div>
   </footer>
 </template>
+
+<script>
+import { version } from '../../../package.json'
+
+export default {
+  data() {
+    return {
+      version
+    }
+  }
+}
+</script>
