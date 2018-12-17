b7071d62c6fcd0219b287de00bee776c621edc2b
Brian Zelip <bzelip@gmail.com>
2018-12-17 13:08:56 -0500

    TheFooter.vue: leverage electron  api to open links in the OS browser

--- src/components/global/TheFooter.vue
+++ src/components/global/TheFooter.vue
@@ -2,19 +2,26 @@
   <footer>
     <div>
       <hr>by
-      <a href="http://zelip.me">bzelip</a> |
-      <a href="https://github.com/brianzelip/concats">app source</a> | v{{ version }}
+      <a href="http://zelip.me" @click="openExternal">bzelip</a> |
+      <a href="https://github.com/brianzelip/concats" @click="openExternal">app source</a> | v{{ version }}
     </div>
   </footer>
 </template>
 
 <script>
-import { version } from '../../../package.json'
+import { version } from '../../../package.json';
+import { shell } from 'electron';
 
 export default {
   data() {
     return {
       version
+    }
+  },
+  methods: {
+    openExternal(e) {
+      e.preventDefault();
+      shell.openExternal(e.target.href)
     }
   }
 }
