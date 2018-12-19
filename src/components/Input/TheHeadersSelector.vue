85944cf22966b51f2d033371f4c0c319c2aa5e86
Brian Zelip <bzelip@gmail.com>
2018-12-19 08:18:32 -0500

    HeadersSelector: copy over markup

--- src/components/Input/TheHeadersSelector.vue
+++ src/components/Input/TheHeadersSelector.vue
@@ -1,4 +1,26 @@
-<template></template>
+<template>
+  <div
+    id="headerSelector"
+    style="margin-bottom: 2rem;"
+    v-if="csvInputHeaders.length > 0"
+  >
+    <p style="margin-bottom: 1rem; font-weight: bold">Select fields to be concatenated in order:</p>
+    <div
+      :key="index"
+      style="padding-bottom: .25rem;"
+      v-for="(header, index) in csvInputHeaders"
+    >
+      <input
+        :id="`headers-${index}`"
+        :value="header"
+        name="headers"
+        type="checkbox"
+        v-model="userSelectedHeaders"
+      >
+      <label :for="`headers-${index}`">{{ header }}</label>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {};
