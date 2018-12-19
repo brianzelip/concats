a291981fd2ea01b1cd18eef5b612393fb64c6b5f
Brian Zelip <bzelip@gmail.com>
2018-12-19 08:17:36 -0500

    FileSelector: copy over markup and styles

--- src/components/Input/TheFileSelector.vue
+++ src/components/Input/TheFileSelector.vue
@@ -1,5 +1,35 @@
-<template></template>
+<template>
+  <div style="margin-bottom: 2rem;">
+    <div
+      @dragover="dragover"
+      @drop="drop"
+      class="dropzone"
+    >
+      <label
+        for="fileInput"
+        style="margin-right: 1rem; font-weight: bold"
+      >Select file:</label>
+      <input
+        @change="getInputFile"
+        accept=".csv, .tsv"
+        id="fileInput"
+        name="fileInput"
+        text="Browse"
+        type="file"
+      >
+      <p style="margin: .5rem 0; font-style: italic">or</p>
+      <p style="margin: 0; font-weight: bold">Drag and drop file here</p>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {};
 </script>
+
+<style>
+.dropzone {
+  padding: 1rem;
+  border-style: dashed;
+}
+</style>
