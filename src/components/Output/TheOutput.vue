79a3abdbc0bc1750aa2a26bc08638dd34cdc4c99
Brian Zelip <bzelip@gmail.com>
2018-12-19 08:20:32 -0500

    Output: copy over markup and register prop

--- src/components/Output/TheOutput.vue
+++ src/components/Output/TheOutput.vue
@@ -1,5 +1,21 @@
-<template></template>
+<template>
+  <div
+    id="output"
+    style="margin-bottom: 2rem;"
+    v-if="userSelectedHeaders.length > 0"
+  >
+    <p style="font-weight: bold;">Your field selection is:</p>
+    <ol>
+      <li
+        :key="index"
+        v-for="(header, index) in userSelectedHeaders"
+      >{{ header }}</li>
+    </ol>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  props: ["userSelectedHeaders"]
+};
 </script>
