d33ce3270666848b11cc545d6712c1c00793917c
Brian Zelip <bzelip@gmail.com>
2018-12-19 16:52:42 -0500

    TheInput: swap out old markup for new FileSelector component, have two-way data flow between parent and child via vue props and custom events

--- src/components/Input/TheInput.vue
+++ src/components/Input/TheInput.vue
@@ -1,30 +1,6 @@
 <template>
   <section style="margin-bottom: 2rem;">
-    <div
-      id="fileSelector"
-      style="margin-bottom: 2rem;"
-    >
-      <div
-        @dragover="dragover"
-        @drop="drop"
-        class="dropzone"
-      >
-        <label
-          for="fileInput"
-          style="margin-right: 1rem; font-weight: bold"
-        >Select file:</label>
-        <input
-          @change="getInputFile"
-          accept=".csv, .tsv"
-          id="fileInput"
-          name="fileInput"
-          text="Browse"
-          type="file"
-        >
-        <p style="margin: .5rem 0; font-style: italic">or</p>
-        <p style="margin: 0; font-weight: bold">Drag and drop file here</p>
-      </div>
-    </div>
+    <TheFileSelector v-on:file-input="getInputFile"></TheFileSelector>
     <div
       id="headerSelector"
       style="margin-bottom: 2rem;"
