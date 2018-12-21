502639ffe1c2b897c4077a5819c47eae49bd7105
Brian Zelip <bzelip@gmail.com>
2018-12-21 06:37:15 -0500

    TheHeader, TheFooter: rmdir src/components/global, put header and footer in src/components, update path in Footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer>
+    <div>
+      <hr>by
+      <a
+        @click="openExternal"
+        href="http://zelip.me"
+      >bzelip</a> |
+      <a
+        @click="openExternal"
+        href="https://github.com/brianzelip/concats"
+      >app source</a>
+      | v{{ version }}
+    </div>
+  </footer>
+</template>
+
+<script>
+import { version } from "../../package.json";
+import { shell } from "electron";
+
+export default {
+  data() {
+    return {
+      version
+    };
+  },
+  methods: {
+    openExternal(e) {
+      e.preventDefault();
+      shell.openExternal(e.target.href);
+    }
+  }
+};
+</script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1>concats</h1>
+    <p>output a single-column csv file of rows of concatenated fields from an input tsv/csv file</p>
+  </header>
+</template>
+
+<style scoped>
+h1 {
+  margin: 0;
+  line-height: 1;
+}
+p {
+  margin: 0 0 2rem 0;
+}
+</style>
