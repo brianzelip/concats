262165335368a05679cf6cf4f8bbf1d3c3ff7462
Brian Zelip <bzelip@gmail.com>
2018-12-21 05:38:32 -0500

    HeadersSelection: create new component to abstract away the rendering of the HeadersSelector for user confirmation

---
+++ src/components/Input/TheHeadersSelection.vue
@@ -0,0 +1,18 @@
+<template>
+  <section style="margin-bottom: 2rem;">
+    <p style="font-weight: bold;">Your field selection is:</p>
+    <ol>
+      <li
+        :key="index"
+        v-for="(header, index) in headers"
+      >{{ header }}</li>
+    </ol>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ["headers"]
+};
+</script>
+
