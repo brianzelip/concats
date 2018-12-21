ab87b84c640b487dda639124f6207f4008f58c66
Brian Zelip <bzelip@gmail.com>
2018-12-21 05:11:11 -0500

    TheControls: create component that holds control buttons, app now works with components 🎉

---
+++ src/components/Input/TheControls.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    id="controls"
+    style="margin-bottom: 2rem;"
+  >
+    <button
+      :disabled="submitted"
+      @click="$emit('input-submitted')"
+    >Submit</button>
+    <button
+      @click="$emit('reset-app')"
+      v-if="csvOutput.length > 0"
+    >RESET DATA</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["submitted", "csvOutput"]
+};
+</script>
+
