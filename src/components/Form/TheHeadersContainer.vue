23ff9560b497314914ccf032c3a3620840b6161b
Brian Zelip <bzelip@gmail.com>
2018-12-26 14:21:29 -0500

    TheHeadersContainer: create new flex container component for headers selector and selection components

---
+++ src/components/Form/TheHeadersContainer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="flex-container">
+    <TheHeadersSelector
+      :headers="headers"
+      class="flex-item"
+      v-on:user-selected-headers-change="updateUserSelectedHeaders"
+    ></TheHeadersSelector>
+
+    <TheHeadersSelection
+      :headers="userSelectedHeaders"
+      class="flex-item"
+    ></TheHeadersSelection>
+  </div>
+</template>
+
+<script>
+import TheHeadersSelector from "./TheHeadersSelector.vue";
+import TheHeadersSelection from "./TheHeadersSelection.vue";
+
+export default {
+  data() {
+    return {
+      userSelectedHeaders: []
+    };
+  },
+  components: {
+    TheHeadersSelector,
+    TheHeadersSelection
+  },
+  props: ["headers"],
+  methods: {
+    updateUserSelectedHeaders(data) {
+      this.userSelectedHeaders = data;
+      this.$emit("user-selected-headers-change", data);
+    }
+  }
+};
+</script>
+
+
+<style>
+.flex-container {
+  display: flex;
+  flex-flow: wrap;
+}
+.flex-item {
+  flex: 0 0 50%;
+}
+</style>
