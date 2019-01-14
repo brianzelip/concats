e168158f2a6d35fdc6c0611093d981e469f8ce61
Brian Zelip <bzelip@gmail.com>
2019-01-14 14:29:14 -0500

    menus/info.js: refactor out Info menu from background.js into its own modules; expects showAbout() and determines whether or not to showAbout based on platform

---
+++ src/menus/info.js
@@ -0,0 +1,28 @@
+import { app, shell } from 'electron';
+
+import { homepage } from '../../package.json';
+
+export default showAbout => {
+  const submenu = [
+    {
+      label: 'Go to concats homepage →',
+      click() {
+        shell.openExternal(homepage);
+      }
+    }
+  ];
+
+  if (process.platform !== 'darwin') {
+    submenu.unshift({
+      label: `About ${app.getName()}`,
+      click() {
+        showAbout();
+      }
+    });
+  }
+
+  return {
+    label: 'Info',
+    submenu
+  };
+};
