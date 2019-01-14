bf37d319582228119f33619237af2f636d619b6f
Brian Zelip <bzelip@gmail.com>
2019-01-14 14:22:59 -0500

    menus/darwin.js: refactor out darwin app menu from background.js into its own file

---
+++ src/menus/darwin.js
@@ -0,0 +1,23 @@
+const { app } = require('electron');
+
+export default showAbout => {
+  return {
+    label: app.getName(),
+    submenu: [
+      {
+        label: 'About concats',
+        click() {
+          showAbout();
+        }
+      },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  };
+};
