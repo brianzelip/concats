126d4612b2c9d379008c726f29f06a881be8a132
Brian Zelip <bzelip@gmail.com>
2019-01-15 11:54:29 -0500

    menu.js: import iconPath in attempt to make showAbout icon display on windows builds

--- src/menus/menu.js
+++ src/menus/menu.js
@@ -1,7 +1,6 @@
-import { resolve } from 'path';
-
 import { app, Menu, dialog } from 'electron';
 
+import icon from '../iconPath';
 import fileMenu from './_file';
 import viewMenu from './_view';
 import infoMenu from './_info';
@@ -11,15 +10,13 @@ export default BrowserWindow => {
   const currentYear = new Date().getFullYear();
 
   const showAbout = () => {
-    // __dirname is actually concats/dist_electron, NOT concats/src/menus
-    // console.log(resolve(__dirname, '../'));
     dialog.showMessageBox({
       type: 'info',
       title: `About ${app.getName()}`,
       message: `${app.getName()} v${app.getVersion()}`,
       detail: `See Info menu for source code\n\nCopyright © 2018-${currentYear} Brian Zelip`,
       buttons: [],
-      icon: resolve(__dirname, '../build/icon.png')
+      icon
     });
   };
 
