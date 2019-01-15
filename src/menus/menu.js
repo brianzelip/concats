118c3995dc6cc5aaeee36e260285af07929c74af
Brian Zelip <bzelip@gmail.com>
2019-01-15 06:59:12 -0500

    menu.js: finalize showAbout panel info, except the icon!

--- src/menus/menu.js
+++ src/menus/menu.js
@@ -1,4 +1,6 @@
-import { Menu, dialog } from 'electron';
+import { app, Menu, dialog } from 'electron';
+
+import { homepage } from '../../package.json';
 
 import fileMenu from './file';
 import viewMenu from './view';
@@ -6,12 +8,14 @@ import infoMenu from './info';
 import darwinMenu from './darwin';
 
 export default BrowserWindow => {
+  const currentYear = new Date().getFullYear();
+
   const showAbout = () => {
     dialog.showMessageBox({
       type: 'info',
-      title: 'About concats',
-      message: 'This is the message!',
-      detail: 'This is the detail.',
+      title: `About ${app.getName()}`,
+      message: `${app.getName()} v${app.getVersion()}`,
+      detail: `Source code available at\n${homepage}\n\nCopyright © 2018-${currentYear} Brian Zelip`,
       buttons: []
     });
   };
