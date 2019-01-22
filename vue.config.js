2e44c663978f9c5df2b7d715c9e726718a00352d
Brian Zelip <bzelip@gmail.com>
2019-01-22 12:01:33 -0500

    vue.config.js: update entry points for main and renderer

--- vue.config.js
+++ vue.config.js
@@ -11,12 +11,20 @@ module.exports = {
 
     // add replacement loader(s)
     svgRule.use('vue-svg-loader').loader('vue-svg-loader');
+
+    // change webpack entry point from src/main.js
+    // code via https://stackoverflow.com/a/52773981/2145103
+    // ...figured it out after a bit of trials and trib,
+    // ...plus `vue inspect` on the cli
+
+    // clear the existing entry point
+    config.entry('app').clear();
+    // add your custom entry point
+    config.entry('app').add('./src/renderer/main.js');
   },
   pluginOptions: {
     electronBuilder: {
-      builderOptions: {
-        // options placed here will be merged with default configuration and passed to electron-builder
-      }
+      mainProcessFile: './src/main/background.js'
     }
   }
 };
