3f6cf8f0be45df777972b8004cd3677284c8278c
Brian Zelip <bzelip@gmail.com>
2019-01-24 14:57:00 -0500

    fileHelper.js: Renames exports, deletes callback exports

--- src/shared/fileHelper.js
+++ src/shared/fileHelper.js
@@ -1,6 +1,4 @@
-import fs from 'fs';
-
-const util = {
+const fCheck = {
   fileIsValid(file) {
     // analyze file extension approach via https://stackoverflow.com/a/22864057/2145103
     function hasExtension(file) {
@@ -31,31 +29,9 @@ const util = {
   }
 };
 
-const options = {
+const openDialogOptions = {
   title: 'Select a data file',
   properties: ['openFile']
 };
 
-const mainCB = (filePaths, BrowserWindow) => {
-  if (filePaths != undefined) {
-    fs.readFile(filePaths[0], 'utf-8', function(err, fileAsString) {
-      if (err) {
-        throw err;
-      }
-      BrowserWindow.webContents.send('file-input', fileAsString);
-    });
-  }
-};
-
-const rendererCB = filePaths => {
-  console.log('vm from openDialog.js is:::', vm);
-  if (filePaths != undefined) {
-    util.fileIsValid(filePaths[0])
-      ? fs.readFile(filePaths[0], 'utf-8', function(err, fileAsString) {
-          vm.$emit('file-input', fileAsString);
-        })
-      : console.log(util.errorMsg(filePaths[0]));
-  }
-};
-
-export { options, mainCB, rendererCB };
+export { fCheck, openDialogOptions };
