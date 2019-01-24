8dc165b0f83c28fb4d7fe8b60c3c9b2a7d750c32
Brian Zelip <bzelip@gmail.com>
2019-01-24 15:06:30 -0500

    FileSelector: Rm unused file helper methods

--- src/renderer/components/Form/TheFileSelector.vue
+++ src/renderer/components/Form/TheFileSelector.vue
@@ -60,36 +60,7 @@ export default {
       const fileName = e.dataTransfer.files[0].name;
       fCheck.fileIsValid(fileName)
         ? this.$emit("file-input", e.dataTransfer.files[0])
-        : console.log(this.errorMsg(fileName));
-    },
-    fileIsValid(file) {
-      // analyze file extension approach via https://stackoverflow.com/a/22864057/2145103
-
-      function hasExtension(file) {
-        return (
-          file
-            .split("/")
-            .pop()
-            .split(".").length > 1
-        );
-      }
-      function getExtension(file) {
-        return file
-          .split("/")
-          .pop()
-          .split(".")
-          .pop();
-      }
-      function isExtensionWeLike(ext) {
-        return ext === "csv" || ext === "tsv";
-      }
-      return (hasExtension(file) && isExtensionWeLike(getExtension(file))) ||
-        !hasExtension(file)
-        ? true
-        : false;
-    },
-    errorMsg(fileName) {
-      return `File selected had wrong extension (${fileName}); please select a csv/tsv file or a data file with no extension.`;
+        : console.log(fCheck.errorMsg(fileName));
     }
   },
   components: {
